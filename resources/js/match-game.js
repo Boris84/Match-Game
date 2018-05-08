1292ceddd2f12be7ee8d85d02f409d6694d54535
U-ADMIN-VAIO\Admin <simon.789@hotmail.co.uk>
2018-05-08 21:48:32 +0100

    update 18 Match-Game js flipCard

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -55,13 +55,37 @@ MatchGame.renderCards = function(cardValues, $game) {
  */
 
 MatchGame.flipCard = function($card, $game) {
+  
   if ($card.data('isFlipped') === true) {
 
      return ;
  }
-     
+     var fCards = $game.data('flippedCards');
      $card.css('background-color', $card.data('color'));
      $card.text($card.data('value'));
      $card.data('isFlipped', true);
      $game.data('flippedCards').push($card);
+     
+  if (fCards.length === 2) {
+
+  if (fCards[0].data('value') === fCards[1].data('value')) {
+      
+     fCards[0].css('background-color', rgb(153, 153, 153));
+     fCards[0].css('color', rgb(204, 204, 204));
+     fCards[1].css('background-color', rgb(153, 153, 153));
+     fCards[1].css('color', rgb(204, 204, 204));
+    
+ } else if (fCards[0].data('value') !== fCards[1].data('value')) {
+
+     fCards[0].css('background-color');
+     fCards[0].text('');
+     fCards[0].data('isFlipped', false);
+     fCards[1].css('background-color');
+     fCards[1].text('');
+     fCards[1].data('isFlipped', false);
+
+ }
+
+     }
+
 }
